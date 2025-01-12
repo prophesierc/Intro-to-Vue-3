9fa537fa523bc03e28921bf4348bcc17d060b375
ProphesierC <bwknowles98@gmail.com>
2025-01-11 23:54:34 -0500

    Lesson 10 + Challenge

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        updateCart(id) {
+            this.cart.push(id);
+        },
+        // intention is to filter for the item id and remove the most recent of that id from the array
+        removeFromCart(id) {
+            if (this.cart.includes(id))
+            {
+                this.cart = this.cart.filter((x) => x.value = id) 
+                this.cart.pop()
+            }
+            return this.cart
+        }
+    }
 })
